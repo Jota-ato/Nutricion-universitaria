cc01b357e049e29b71d0ca513a7478644edf4d3e
Jota-ato <julioc-01@outlook.com>
2026-02-19 17:27:10 -0600

    debug: add registration button to debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
     return (
         <div className="flex items-center justify-center min-h-screen">
             <h1>Nutricion Universitaria</h1>
+            <Button>
+                <Link
+                    href={'/registro'}
+                >
+                    Registrarse
+                </Link>
+            </Button>
         </div>
     );
 }
