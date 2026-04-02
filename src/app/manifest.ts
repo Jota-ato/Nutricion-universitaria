800416fcc6924db9de79b2c8cfd02555d5e7b6b7
Jota-ato <julioc-01@outlook.com>
2026-04-02 15:43:23 -0600

    feat: add new icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,15 +11,15 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#ffffff',
         icons: [
             {
-                src: '/logo.svg',
-                sizes: 'any',
-                type: 'image/svg+xml',
-                purpose: 'any', 
+                src: '/logo.png',
+                sizes: '192x192',
+                type: 'image/png',
+                purpose: 'any',
             },
             {
-                src: '/logo-maskable.svg',
-                sizes: 'any',
-                type: 'image/svg+xml',
+                src: '/logo-maskable.png',
+                sizes: '512x512',
+                type: 'image/png',
                 purpose: 'maskable',
             },
         ]
